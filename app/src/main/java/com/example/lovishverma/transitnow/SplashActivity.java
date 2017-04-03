27c8877dd7838589b9de0be8439a7a2f72644c8b
Lovish Verma <vermalovish1@gmail.com>
2017-04-04 01:02:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lovishverma/transitnow/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.lovishverma.transitnow;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread t = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        };
+        t.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
